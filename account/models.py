0801b8a29ba3f4556b7ead1dcfed7b617af7a1ec
kmush12 <k.grzyb112@gmail.com>
2022-01-26 22:37:23 +0100

    Modele

--- account/models.py
+++ account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Account(models.Model):
+    user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg')
+    friends = models.ManyToManyField(User, related_name='user_friends', blank=True)
+
+
+class Request(models.Model):
+    user_from = models.ForeignKey(User, related_name='user_from', on_delete=models.CASCADE)
+    user_to = models.ForeignKey(User,  related_name='user_to', on_delete=models.CASCADE)
--- post/models.py
+++ post/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+    photo = models.ImageField(upload_to='post_photos', null=True)
+    date = models.DateTimeField(default=timezone.now)
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+    date = models.DateTimeField(default=timezone.now)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
