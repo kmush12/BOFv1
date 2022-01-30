2f3b8556c37992938b38fda271b2141b5dcac00e
kmush12 <k.grzyb112@gmail.com>
2022-01-30 20:07:33 +0100

    Znajomi

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from account.views import register_view, login_view,  logout_view, search_view
+from account.views import register_view, login_view,  logout_view, search_view, requests_list_view, send_request_view, accept_request_view, decline_request_view
 from django.urls import path
 
 app_name = "account"
@@ -7,4 +7,8 @@ urlpatterns = [
         path('logout/', logout_view, name='logout-view'),
         path('register/', register_view, name='register-view'),
         path('search/', search_view, name='search-view'),
+        path('requests/', requests_list_view, name='requests-list-view'),
+        path('send-request/<int:id>/', send_request_view, name='send-request-view'),
+        path('accept-request/<int:id>/', accept_request_view, name='accept-request-view'),
+        path('decline-request/<int:id>/', decline_request_view, name='decline-request-view'),
     ]
